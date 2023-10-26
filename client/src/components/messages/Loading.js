68490b5eb8a6ecc4a295d46d48af7f75ddbc0ea4
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2023-10-27 00:45:53 +0200

    Adding messages

---
+++ client/src/components/messages/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "../../styles/messages/loading.module.css"
+
+const Loading = () => {
+    return (
+        <div className={styles.mainContainer}>
+            <span>Loading...</span>
+        </div>
+    )
+}
+
+export default Loading
