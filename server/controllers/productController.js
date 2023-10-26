f0114d5caf894dfdf114e4641e96c9a6c0751063
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2023-10-27 00:34:53 +0200

    Update product and product controller

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -2,14 +2,7 @@ import Product from "../models/ProductModel.js";
 
 //GET ALL PRODUCTS
 export const getAllProducts = async (req, res) => {
-    const keyword = req.query.keyword ? {
-        name: {
-            $regex: req.query.keyword,
-            $options: "i"
-        },
-    }
-        : {}
-    const products = await Product.find({ ...keyword })
+    const products = await Product.find({})
     res.json(products);
 }
 
--- server/data/Products.js
+++ server/data/Products.js
@@ -1,112 +1,82 @@
 const products = [
     {
-        title: "Margherita Pizza",
-        price: 15.2,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/1.jpg"
+        image: "/images/products/by.png",
+        title: "Žuti šorc",
+        price: 13.28,
+        description: "Markonov šorc u žutoj boji ukrašen grbom Šarić Struge. Ovakvi šorcevi korišteni su za veslanje posljednja dva maratona. Iznimno kvalitetni i udboni.",
     },
     {
-        title: "Cheese n Corn Pizza",
-        price: 13.40,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/2.jpg"
+        image: "/images/products/bw.png",
+        title: "Bijeli šorc",
+        price: 13.28,
+        description: "Markonov šorc u bijeloj boji ukrašen grbom Šarić Struge. Ovakvi šorcevi korišteni su uglavnom od strane naših veslača na treninzima. Iznimno kvalitetni i udboni.",
     },
     {
-        title: "Cheese n Tomato Pizza",
-        price: 13.70,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/3.jpg"
+        image: "/images/products/nm2022.png",
+        title: "Navijačka majica 2022",
+        price: 6.64,
+        description: "Navijačke majice napravljene za 25. maraton lađa. Majice su se u početku radile isključivo za veslače, a sada su dostupne i za vas putem našeg Web Shopa. Najveći sponzor je Vinarija Vučica",
     },
     {
-        title: "Double Cheese Margherita Pizza",
-        price: 18.30,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/4.jpg"
+        image: "/images/products/nm2021.png",
+        title: "Navijačka majica 2021",
+        price: 6.64,
+        description: "Majice napravljene za 24. maraton lađa. Jedan od najvećih sponzora ove godine su bile Hrvatske Šume. Iz tog razloga tiskano i prodano puno ovakvih majica, još uvijek su dostupne u našem Web Shopu, ali u manjem broju.",
     },
     {
-        title: "Fresh Veggie Pizza",
-        price: 12.10,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/5.jpg"
+        image: "/images/products/nm2020.png",
+        title: "Navijačka majica 2020",
+        price: 6.64,
+        description: "Majice tiskane u godini povratka ekipe, za 23. maraton lađa. Tiskane su manjem broju jer su sponzori bili ograničeni. Dostupno u velikim količina putem našeg Web Shopa.",
     },
     {
-        title: "Chicken Sausage Pizza",
-        price: 14.80,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/6.jpg"
+        image: "/images/products/nm2017.png",
+        title: "Navijačka majica 2017",
+        price: 4.64,
+        description: "Stari dres iz 2017. godine. Ostao je mali broj majica. S obzirom da se radi o starijem kroju i dizajnu snižene su cijene.",
     },
     {
-        title: "Chicken Golden Delight Pizza",
-        price: 18.20,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/7.jpg"
+        image: "/images/products/vm2022.png",
+        title: "Slim fit veslačka majica 2022",
+        price: 40,
+        description: "Slim fit majica s kojom su naši lađari izveslali 25. maraton. U početku tiskan je točan broj majica za lađare. Ukoliko naručite putem Web Shopa majica se posebno tiska pa je cijena veća.",
     },
     {
-        title: "Veg Burger",
-        price: 16.10,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/8.jpg"
+        image: "/images/products/vm2021.png",
+        title: "Slim fit veslačka majica 2021",
+        price: 40,
+        description: "Slim fit majica s kojom su naši lađari izveslali 24. maraton. U početku tiskan je točan broj majica za lađare. Ukoliko naručite putem Web Shopa majica se posebno tiska pa je cijena veća.",
     },
     {
-        title: "Crunchy Veg Burger",
-        price: 16.10,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/9.jpg"
+        image: "/images/products/vm2020.png",
+        title: "Slim fit veslačka majica 2020",
+        price: 30,
+        description: "Slim fit majica s kojom su naši lađari izveslali 23. maraton. U početku tiskan je točan broj majica za lađare. Cijena snižena",
     },
     {
-        title: "Chicken Burger",
-        price: 16.50,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/10.jpg"
+        image: "/images/products/carape1.png",
+        title: "Bijele čarape",
+        price: 4,
+        description: "Paket od tri para bijelih čarapa sa logom ULŠS.",
     },
     {
-        title: "Crunchy Chicken Burger",
-        price: 16.50,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/11.jpg"
+        image: "/images/products/carape2.png",
+        title: "Žute čarape",
+        price: 4,
+        description: "Paket od tri para žutih čarapa sa logom ULŠS.",
     },
     {
-        title: "Lamb Burger",
-        price: 17.00,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/12.jpg"
+        image: "/images/products/Žuta-winner-min.png",
+        title: "Trenerka (gornji i donji dio)",
+        price: 66.67,
+        description: "Gornji i donji dio žuto crne trenerke sa logom udruge lađara ŠS. Također se posebno tiskaju inicijali pa je cijena malo povišena.",
     },
     {
-        title: "Veg Grilled Sandwich",
-        price: 6.30,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/13.jpg"
+        image: "/images/products/tene1.png",
+        title: "Tenisice",
+        price: 66.67,
+        description: "Tenisice sa logom ULŠS. Dizajn napravljen za buduće generacije. Ne radi se o veslačkim tenisicama.",
     },
-    {
-        title: "Chicken Grilled Sandwich",
-        price: 6.30,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/14.jpg"
-    },
-    {
-        title: "Cheese Grilled Sandwich",
-        price: 5.00,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/15.jpg"
-    },
-    {
-        title: "Tortilla Wrap",
-        price: "10.50",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/16.jpg"
-    },
-    {
-        title: "French Fries",
-        price: 4.90,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/17.jpg"
-    },
-    {
-        title: "Chicken Nuggets",
-        price: 7.30,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-        image: "/images/18.jpg"
-    }
 ]
 
 export default products
