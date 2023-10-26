82266c74d465c6610afc5d8ccfe9652f81c450ce
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2023-10-27 00:47:14 +0200

    Update Fan Shop

---
+++ client/src/pages/fan-shop/Shop.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import * as BsIcons from "react-icons/bs"
+import * as AiIcons from "react-icons/ai"
+import { useDispatch, useSelector } from "react-redux"
+import { listProduct } from '../../Redux/Actions/ProductActions'
+import styles from "../../styles/fan-shop/shop.module.css"
+import Loading from '../../components/messages/Loading'
+import Error from '../../components/messages/Error'
+
+function Shop() {
+
+    const [query, setQuery] = useState("")
+
+    const dispatch = useDispatch()
+
+    const productList = useSelector((state) => state.productList);
+    const { loading, error, products } = productList;
+
+    useEffect(() => {
+        dispatch(listProduct());
+    }, [dispatch])
+
+    return (
+        <div className={styles.shopContainer}>
+            <div className={styles.wraper}>
+                <div className={styles.left}>
+                    <span>HR</span>
+                    <label>
+                        <input onChange={event => setQuery(event.target.value)} />
+                        <BsIcons.BsSearch />
+
+                    </label>
+                </div>
+                <div className={styles.center}>
+                    <h1>Službeni Web Shop</h1>
+                </div>
+                <div className={styles.right}>
+                    <Link to="cart">
+                        <AiIcons.AiOutlineShoppingCart className={styles.cart} />
+                    </Link>
+
+                    <span></span>
+                </div>
+            </div>
+            <hr />
+            <div className={styles.container}>
+                {
+                    loading ? (<Loading />) : error ? (<Error>Something went wrong</Error>)
+                        :
+                        (
+                            <>
+                                {products.filter(item => {
+                                    if (query === "") {
+                                        return item;
+                                    } else if (item.title.toLowerCase().includes(query.toLowerCase())) {
+                                        return item;
+                                    }
+                                }).map((item) => {
+                                    return (
+                                        <div className={styles.main} key={item.id}>
+                                            <img src={item.img} alt={item.title} />
+                                            <div className={styles.info}>
+                                                <div className={styles.icon}>
+                                                    <AiIcons.AiOutlineShoppingCart />
+                                                </div>
+                                                <Link to={`products/${item._id}`}>
+                                                    <div className={styles.icon}>
+                                                        <AiIcons.AiOutlineSearch />
+                                                    </div>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    )
+                                })}
+                            </>
+                        )
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Shop
---
+++ client/src/pages/fan-shop/SingleProduct.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+
+function SingleProduct() {
+    // const navigate = useNavigate()
+    // const { id } = useParams()
+
+    // return (
+    //     <div className={styles.item}>
+    //         <div className={styles.wraper}>
+    //             <div className={styles.center}>
+    //                 <h2>Službeni Web Shop</h2>
+    //             </div>
+    //         </div>
+    //         <hr />
+
+    //         <div className={styles.wrapperContainer}>
+    //             <div className={styles.image}>
+    //                 <img src={img} alt='slika' />
+    //             </div>
+
+    //             <div className={styles.information}>
+    //                 <h1>
+    //                     {title}
+    //                 </h1>
+    //                 <p>
+    //                     {desc}
+    //                 </p>
+    //                 <b>{price} €</b>
+    //                 <form>
+    //                     <label>
+    //                         Veličina:
+    //                         <select>
+    //                             <option>XXS</option>
+    //                             <option>XS</option>
+    //                             <option>S</option>
+    //                             <option>M</option>
+    //                             <option>L</option>
+    //                             <option>XL</option>
+    //                             <option>XXL</option>
+    //                         </select>
+    //                     </label>
+    //                 </form>
+    //                 <div className={styles.add}>
+    //                     <Link to>
+    //                         <button onClick={handleClick} className={styles.basket}>Dodaj u košaricu</button>
+    //                     </Link>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </div>
+    // )
+}
+
+export default SingleProduct
