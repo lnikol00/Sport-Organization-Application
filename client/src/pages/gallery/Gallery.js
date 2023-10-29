ad022323238b3424de8971fca874f6c34637f6b8
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2023-10-29 13:03:49 +0100

    Adding animated routes to gallery

--- client/src/pages/gallery/Gallery.js
+++ client/src/pages/gallery/Gallery.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Data } from './GalleryData'
 import * as AiIcons from "react-icons/ai"
+import AnimatedPage from "../../components/context/AnimatedPage"
 
 import styles from "../../styles/gallery/gallery.module.css"
 
@@ -15,24 +16,26 @@ function Gallery() {
     }
 
     return (
-        <div className={styles.mainContainer}>
-            <div className={styles.heading}>
-                <h3>Galerija <span>Slika</span></h3>
-            </div>
-            <div className={model ? `${styles.model} ${styles.modelOpen}` : `${styles.model}`}>
-                <img src={tempimgSrc} />
-                <AiIcons.AiOutlineClose onClick={() => setModel(false)} />
-            </div>
-            <div className={styles.gallery}>
-                {Data.map((slika, index) => {
-                    return (
-                        <div className={styles.pics} key={index} onClick={() => getImg(slika.imgSrc)}>
-                            <img src={slika.imgSrc} style={{ width: '100%' }} />
-                        </div>
-                    )
-                })}
-            </div>
-        </div >
+        <AnimatedPage>
+            <div className={styles.mainContainer}>
+                <div className={styles.heading}>
+                    <h3>Galerija <span>Slika</span></h3>
+                </div>
+                <div className={model ? `${styles.model} ${styles.modelOpen}` : `${styles.model}`}>
+                    <img src={tempimgSrc} />
+                    <AiIcons.AiOutlineClose onClick={() => setModel(false)} />
+                </div>
+                <div className={styles.gallery}>
+                    {Data.map((slika, index) => {
+                        return (
+                            <div className={styles.pics} key={index} onClick={() => getImg(slika.imgSrc)}>
+                                <img src={slika.imgSrc} style={{ width: '100%' }} />
+                            </div>
+                        )
+                    })}
+                </div>
+            </div >
+        </AnimatedPage>
     )
 }
 
