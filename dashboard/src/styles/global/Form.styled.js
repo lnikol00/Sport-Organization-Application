efcac4afb1c556f8de2ae015efef798aa7425488
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2023-11-11 22:04:37 +0100

    adding styles

---
+++ dashboard/src/styles/global/Form.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const FormContainer = styled.form`
+
+`
+
+export const InputContainer = styled.input`
+
+`
---
+++ dashboard/src/styles/global/Text.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+
+`
+
+export const Paragraph = styled.p`
+
+`
+
+export const Span = styled.span`
+
+`
