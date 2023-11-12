18664e3b1f08856f447aa92ecb7311499f271f76
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2023-11-12 19:35:58 +0100

    adding font size to Text styles

--- dashboard/src/styles/global/Text.styled.js
+++ dashboard/src/styles/global/Text.styled.js
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 
 export const Title = styled.h1`
-
+    font-size: ${(props) => (props.fontSize)};
 `
 
 export const Paragraph = styled.p`
-
+    font-size: ${(props) => (props.fontSize)};
 `
 
 export const Span = styled.span`
-
+    font-size: ${(props) => (props.fontSize)};
+    padding: ${(props) => (props.padding)};
 `
