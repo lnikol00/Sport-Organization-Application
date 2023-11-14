97c6cc0c4552298cad3aee31c5e9d6fe4c293bf2
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2023-11-14 22:53:17 +0100

    update dashboard page

--- dashboard/src/pages/dashboard/Dashobard.js
+++ dashboard/src/pages/dashboard/Dashobard.js
@@ -1,10 +1,115 @@
-import React from 'react'
-import { DashboardContainer } from '../../styles/pages/Dashobard.styled'
+import React, { useState } from 'react'
+import * as FaIcons from "react-icons/fa"
+import {
+    DashboardContainer,
+    Stats,
+    Container,
+    Wrapper,
+    Charts,
+    Orders
+} from '../../styles/pages/Dashobard.styled'
+import {
+    Icon,
+    Quantity,
+    Title
+} from '../../styles/global/Text.styled'
 
 function Dashobard() {
     return (
         <DashboardContainer>
-            Dashobard
+            <Title
+                fontSize="1.7em"
+                $openNavbar="block"
+            >
+                Dashboard
+            </Title>
+            <Stats>
+                <Container $width="33%" $alignItems="center">
+                    <Icon $fontSize="20px">
+                        <FaIcons.FaEuroSign />
+                    </Icon>
+                    <Wrapper>
+                        <Title
+                            $fontSize="15px"
+                            $openNavbar="block"
+                        >
+                            Ukupna zarada
+                        </Title>
+                        <Quantity
+                            $fontSize="15px"
+                        >
+                            132543
+                        </Quantity>
+                    </Wrapper>
+                </Container>
+
+                <Container $width="33%" $alignItems="center">
+                    <Icon $fontSize="20px">
+                        <FaIcons.FaShoppingBag />
+                    </Icon>
+                    <Wrapper>
+                        <Title
+                            $fontSize="15px"
+                            $openNavbar="block"
+                        >
+                            Ukupno narudžbi
+                        </Title>
+                        <Quantity
+                            $fontSize="15px"
+                        >
+                            12
+                        </Quantity>
+                    </Wrapper>
+                </Container>
+
+                <Container $width="33%" $alignItems="center">
+                    <Icon $fontSize="20px">
+                        <FaIcons.FaShoppingCart />
+                    </Icon>
+                    <Wrapper>
+                        <Title
+                            $fontSize="15px"
+                            $openNavbar="block"
+                        >
+                            Ukupno proizvoda
+                        </Title>
+                        <Quantity
+                            $fontSize="15px"
+                        >
+                            10
+                        </Quantity>
+                    </Wrapper>
+                </Container>
+            </Stats>
+
+            <Charts>
+                <Container $width="50%" $flexDirection="column" $alignItems="flex-start">
+                    <Title
+                        $fontSize="20px"
+                        $openNavbar="block"
+                    >
+                        Sale statistics
+                    </Title>
+                </Container>
+                <Container $width="50%" $flexDirection="column" $alignItems="flex-start">
+                    <Title
+                        $fontSize="20px"
+                        $openNavbar="block"
+                    >
+                        Product statistic
+                    </Title>
+                </Container>
+            </Charts>
+
+            <Orders>
+                <Title
+                    $fontSize="20px"
+                    $openNavbar="block"
+                >
+                    Latest Orders
+                </Title>
+            </Orders>
+
         </DashboardContainer>
     )
 }
--- dashboard/src/styles/pages/Dashobard.styled.js
+++ dashboard/src/styles/pages/Dashobard.styled.js
@@ -1,5 +1,57 @@
 import styled from "styled-components";
 
 export const DashboardContainer = styled.div`
-    
+    padding: 30px 50px;
+`
+
+export const Stats = styled.div`
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    gap:30px;
+    padding: 20px 0;
+
+    @media screen and (max-width: 900px) {
+        flex-direction: column;
+        gap: 20px;
+    }
+
+`
+
+export const Container = styled.div`
+    display:flex;
+    justify-content:flex-start;
+    align-items:${(props) => (props.$alignItems)};
+    flex-direction:${(props) => (props.$flexDirection)};
+    gap: 15px;
+    border:1px solid ${(props) => (props.theme.color)};
+    width:${(props) => (props.$width)};
+    padding: 15px;
+
+    @media screen and (max-width: 1200px) {
+        width:100%;
+    }
+`
+
+export const Wrapper = styled.div`
+
+`
+
+export const Charts = styled.div`
+    display: flex;
+    justify-content:center;
+    align-items:center;
+    gap:15px;
+    padding: 0 0 20px;
+
+
+    @media screen and (max-width: 1200px) {
+        flex-direction: column;
+    }
+`
+
+export const Orders = styled.div`
+    border:1px solid ${(props) => (props.theme.color)};
+    width:100%;
+    padding: 15px;
 `
