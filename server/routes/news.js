8b4a9b1d20fd06527fee3728f37875054d0b675c
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2023-11-15 13:50:20 +0100

    adding comments to routes

--- server/routes/news.js
+++ server/routes/news.js
@@ -4,8 +4,10 @@ import * as newsController from "../controllers/newsController.js";
 
 const newsRoute = express.Router();
 
+//ALL NEWS
 newsRoute.get("/", asyncHandler(newsController.getAllNews));
 
+//UNIQUE NEWS
 newsRoute.get("/:id", asyncHandler(newsController.getSingleNews));
 
 export default newsRoute;
