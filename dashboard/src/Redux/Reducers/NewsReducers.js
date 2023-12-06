3a73d5563f7e5b775715dd5e304f38c8d90924b4
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2023-12-07 00:56:08 +0100

    editing redux

--- dashboard/src/Redux/Reducers/NewsReducers.js
+++ dashboard/src/Redux/Reducers/NewsReducers.js
@@ -2,9 +2,6 @@ import {
     NEWS_LIST_REQUEST,
     NEWS_LIST_SUCCESS,
     NEWS_LIST_FAIL,
-    NEWS_DETAILS_REQUEST,
-    NEWS_DETAILS_SUCCESS,
-    NEWS_DETAILS_FAIL
 } from "../Constants/NewsConstants";
 
 //NEWS LIST
@@ -15,20 +12,6 @@ export const newsListReducer = (state = { news: [] }, action) => {
         case NEWS_LIST_SUCCESS:
             return { loading: false, news: action.payload };
         case NEWS_LIST_FAIL:
-            return { loading: false, error: action.payload };
-        default:
-            return state;
-    }
-}
-
-//SINGLE NEWS DETAILS
-export const newsDetailsReducer = (state = { news: {} }, action) => {
-    switch (action.type) {
-        case NEWS_DETAILS_REQUEST:
-            return { ...state, loading: true };
-        case NEWS_DETAILS_SUCCESS:
-            return { loading: false, news: action.payload };
-        case NEWS_DETAILS_FAIL:
             return { loading: false, error: action.payload };
         default:
             return state;
