0a18c3ce5bf5ea5d50e7ca62f4967cf7922c93b8
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-03-02 12:07:05 +0100

    Editing login page

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react'
 import styles from "../../styles/login/login.module.css"
-import { motion } from "framer-motion"
 import * as AiIcons from "react-icons/ai"
 import * as BiIcons from "react-icons/bi"
-import logoImage from "../../utils/images/header/grb2.png"
 import AnimatedPage from '../../context/AnimatedPage'
 import { useDispatch, useSelector } from "react-redux"
-import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { login, register } from "../../Redux/Actions/UserAction"
 import Loading from '../../utils/messages/Loading'
 import Error from '../../utils/messages/Error'
@@ -14,6 +12,10 @@ import Error from '../../utils/messages/Error'
 function Login() {
 
     const [change, setChange] = useState(true);
+
+    const handleChange = () => {
+        setChange(!change)
+    }
 
     //Login
     const [name, setName] = useState('')
@@ -45,7 +47,7 @@ function Login() {
     const handleRegisterSubmit = (e) => {
         e.preventDefault();
         dispatch(register(name, email, password));
-        setChange(!change);
+        setChange(true);
         setName("");
         setEmail("");
         setPassword("");
@@ -54,224 +56,93 @@ function Login() {
 
     return (
         <AnimatedPage>
-            <div className={styles.mainContainer}>
-                <div className={styles.wrapperContainer}>
-                    <div className={styles.top}>
-                        <h4>ULŠS</h4>
-                        <img alt='logo' src={logoImage} />
-                    </div>
-                    <div className={styles.center}>
-                        <motion.div className={styles.forms}
-                            animate={{ x: change ? 0 : 360 }}
-                            transition={{ type: "tween", duration: 1 }}
-                        >
-                            {change ?
-                                (
-                                    <div
-                                        className={styles.formContainer}>
-                                        <h3>Prijavi se</h3>
-                                        {
-                                            error && <Error>{error}</Error>
-                                        }
-                                        {
-                                            loading && <Loading />
-                                        }
-                                        <form onSubmit={handleLoginSubmit}>
-                                            <label>
-                                                <AiIcons.AiOutlineMail />
-                                                <input
-                                                    placeholder='Email'
-                                                    type='email'
-                                                    value={email}
-                                                    onChange={(e) => setEmail(e.target.value)}
-                                                    required
-                                                />
-                                            </label>
-                                            <label>
-                                                <AiIcons.AiOutlineLock />
-                                                <input
-                                                    placeholder='Password'
-                                                    type='password'
-                                                    value={password}
-                                                    onChange={(e) => setPassword(e.target.value)}
-                                                    required
-                                                />
-                                            </label>
-                                            <Link to>Forgot your password?</Link>
-                                            <button>PRIJAVI SE</button>
-                                        </form>
-                                    </div>
-                                ) : (
-                                    <div
-                                        className={styles.formContainer}>
-                                        <h3>Kreiraj račun</h3>
-                                        {
-                                            error && <Error>{error}</Error>
-                                        }
-                                        {
-                                            loading && <Loading />
-                                        }
-                                        <form onSubmit={handleRegisterSubmit}>
-                                            <label>
-                                                <BiIcons.BiUser />
-                                                <input
-                                                    placeholder='Name'
-                                                    type='text'
-                                                    value={name}
-                                                    onChange={(e) => setName(e.target.value)}
-                                                    required
-                                                />
-                                            </label>
-                                            <label>
-                                                <AiIcons.AiOutlineMail />
-                                                <input
-                                                    placeholder='Email'
-                                                    type='email'
-                                                    value={email}
-                                                    onChange={(e) => setEmail(e.target.value)}
-                                                    required
-                                                />
-                                            </label>
-                                            <label>
-                                                <AiIcons.AiOutlineLock />
-                                                <input
-                                                    placeholder='Password'
-                                                    type='password'
-                                                    value={password}
-                                                    onChange={(e) => setPassword(e.target.value)}
-                                                    required
-                                                />
-                                            </label>
-                                            <button>REGISTRIRAJ SE</button>
-                                        </form>
-                                    </div>
-                                )
-                            }
-                        </motion.div>
-                        <motion.div
-                            className={change ? `${styles.background} ${styles.right}` : `${styles.background} ${styles.left}`}
-                            animate={{ x: change ? 0 : -540 }}
-                            transition={{ type: "tween", duration: 1 }}
-                        >
+            <div className={styles.bodyContainer}>
+                <div className={styles.mainContainer}>
+                    <div className={change ? `${styles.formContainer} ${styles.singUp}` : `${styles.formContainer} ${styles.singUp} ${styles.activeSignUp}`}>
+                        <form onSubmit={handleRegisterSubmit}>
+                            <h1>Kreiraj Račun</h1>
                             {
-                                change ?
-                                    (
-                                        <div className={styles.messages}>
-                                            <h3>Pozdrav, Prijatelju!</h3>
-                                            <p>Unesi par osobnih podataka i postani član!</p>
-                                            <button onClick={() => setChange(!change)}>Registriraj se</button>
-                                        </div>
-                                    ) : (
-                                        <div className={styles.messages}>
-                                            <h3>Dobrodošli nazad!</h3>
-                                            <p>Da ostanete povezani sa nama, molimo Vas da se prijavite na vaš račun</p>
-                                            <button onClick={() => setChange(!change)}>Prijavi se</button>
-                                        </div>
-                                    )
+                                error && <Error>{error}</Error>
                             }
-                        </motion.div>
+                            {
+                                loading && <Loading />
+                            }
+                            <label>
+                                <BiIcons.BiUser />
+                                <input
+                                    placeholder='Ime'
+                                    type='text'
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    required
+                                />
+                            </label>
+                            <label>
+                                <AiIcons.AiOutlineMail />
+                                <input
+                                    placeholder='Email'
+                                    type='email'
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
+                            </label>
+                            <label>
+                                <AiIcons.AiOutlineLock />
+                                <input
+                                    placeholder='Lozinka'
+                                    type='password'
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                />
+                            </label>
+                            <button>REGISTRIRAJ SE</button>
+                        </form>
                     </div>
-                </div>
-
-                <div className={styles.responsiveContainer}>
-                    <div className={styles.container}>
-                        <div className={styles.modes}>
-                            <div
-                                onClick={() => setChange(true)}
-                                className={change ? `${styles.clicked}` : `${styles.notClicked}`}>
-                                <button>Login</button>
+                    <div className={change ? `${styles.formContainer} ${styles.singIn}` : `${styles.formContainer} ${styles.singIn} ${styles.activeSignIn}`}>
+                        <form onSubmit={handleLoginSubmit}>
+                            <h1>Prijavi se</h1>
+                            {
+                                error && <Error>{error}</Error>
+                            }
+                            {
+                                loading && <Loading />
+                            }
+                            <label>
+                                <AiIcons.AiOutlineMail />
+                                <input
+                                    placeholder='Email'
+                                    type='email'
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
+                            </label>
+                            <label>
+                                <AiIcons.AiOutlineLock />
+                                <input
+                                    placeholder='Lozinka'
+                                    type='password'
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                />
+                            </label>
+                            <button>Prijavi se</button>
+                        </form>
+                    </div>
+                    <div className={change ? `${styles.toggleContainer}` : `${styles.toggleContainer} ${styles.activeToggleContainer}`}>
+                        <div className={change ? `${styles.toggle}` : `${styles.toggle} ${styles.activeToggle}`}>
+                            <div className={change ? `${styles.togglePanel} ${styles.toggleRight}` : `${styles.togglePanel} ${styles.toggleRight} ${styles.activeToggleRight}`}>
+                                <h3>Pozdrav, Prijatelju!</h3>
+                                <p>Unesi par podataka i postani član!</p>
+                                <button onClick={handleChange} className={styles.hidden}>Registriraj se</button>
                             </div>
-                            <div
-                                onClick={() => setChange(false)}
-                                className={change ? `${styles.notClicked}` : `${styles.clicked}`}>
-                                <button>Register</button>
-                            </div>
-                        </div>
-                        <div className={styles.center}>
-                            <div className={styles.forms}
-                            >
-                                {change ?
-                                    (
-                                        <div
-                                            className={styles.formContainer}>
-                                            <h3>Prijavi se</h3>
-                                            {
-                                                error && <Error>{error}</Error>
-                                            }
-                                            {
-                                                loading && <Loading />
-                                            }
-                                            <form onSubmit={handleLoginSubmit}>
-                                                <label>
-                                                    <AiIcons.AiOutlineMail />
-                                                    <input
-                                                        placeholder='Email'
-                                                        type='email'
-                                                        value={email}
-                                                        onChange={(e) => setEmail(e.target.value)}
-                                                        required
-                                                    />
-                                                </label>
-                                                <label>
-                                                    <AiIcons.AiOutlineLock />
-                                                    <input
-                                                        placeholder='Lozinka'
-                                                        type='password'
-                                                        value={password}
-                                                        onChange={(e) => setPassword(e.target.value)}
-                                                        required
-                                                    />
-                                                </label>
-                                                <Link to>Forgot your password?</Link>
-                                                <button>PRIJAVI SE</button>
-                                            </form>
-                                        </div>
-                                    ) : (
-                                        <div
-                                            className={styles.formContainer}>
-                                            <h3>Kreiraj račun</h3>
-                                            {
-                                                error && <Error>{error}</Error>
-                                            }
-                                            {
-                                                loading && <Loading />
-                                            }
-                                            <form onSubmit={handleRegisterSubmit}>
-                                                <label>
-                                                    <BiIcons.BiUser />
-                                                    <input
-                                                        placeholder='Ime'
-                                                        type='text'
-                                                        value={name}
-                                                        onChange={(e) => setName(e.target.value)}
-                                                        required
-                                                    />
-                                                </label>
-                                                <label>
-                                                    <AiIcons.AiOutlineMail />
-                                                    <input
-                                                        placeholder='Email'
-                                                        type='email'
-                                                        value={email}
-                                                        onChange={(e) => setEmail(e.target.value)}
-                                                        required
-                                                    />
-                                                </label>
-                                                <label>
-                                                    <AiIcons.AiOutlineLock />
-                                                    <input
-                                                        placeholder='Lozinka'
-                                                        type='password'
-                                                        value={password}
-                                                        onChange={(e) => setPassword(e.target.value)}
-                                                        required
-                                                    />
-                                                </label>
-                                                <button>REGISTRIRAJ SE</button>
-                                            </form>
-                                        </div>
-                                    )
-                                }
+                            <div className={change ? `${styles.togglePanel} ${styles.toggleLeft}` : `${styles.togglePanel} ${styles.toggleLeft} ${styles.activeToggleLeft}`}>
+                                <h3>Dobrodošli nazad!</h3>
+                                <p>Da ostanete povezani sa nama, molimo Vas da se prijavite na vaš račun</p>
+                                <button onClick={handleChange} className={styles.hidden}>Prijavi se</button>
                             </div>
                         </div>
                     </div>
