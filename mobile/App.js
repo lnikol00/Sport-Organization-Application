e51699ec3dfc591ce50f1a0604320eba0a6a39a3
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-03-27 13:23:02 +0100

    Adding login and signup page

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from "./src/pages/login/Login"
+import Signup from './src/pages/login/Signup';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
     return (
-        <View className="bg-red-300 h-[100%] flex justify-center items-center">
-            <Text>Open up App.js to start working on your appsa!</Text>
-            <StatusBar style="auto" />
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='Login' screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Signup" component={Signup} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
+
+export default App;
---
+++ mobile/src/pages/login/Login.js
@@ -0,0 +1,55 @@
+import { useNavigation } from '@react-navigation/native'
+import React from 'react'
+import { Text, View, Image, StatusBar, TextInput, TouchableOpacity } from 'react-native'
+import Animated, { FadeInUp, FadeInDown } from 'react-native-reanimated'
+
+function Login() {
+
+    const navigation = useNavigation()
+
+    return (
+        <View className="bg-white h-full w-full">
+            <StatusBar style="light" />
+            <Image className="h-full w-full absolute" source={require('../../../assets/background.png')} />
+
+            <View className="flex-row justify-around w-full absolute">
+                <Animated.Image entering={FadeInUp.delay(200).duration(1000).springify()} className="h-[225] w-[90]" source={require("../../../assets/light.png")} />
+                <Animated.Image entering={FadeInUp.delay(400).duration(1000).springify()} className="h-[160] w-[65]" source={require("../../../assets/light.png")} />
+            </View>
+
+            <View className="h-full w-full justify-around pt-40 pb-10'">
+                <View className="flex items-center">
+                    <Animated.Text entering={FadeInUp.duration(1000).springify()} className="text-white font-bold tracking-wider text-5xl">
+                        Prijavi se
+                    </Animated.Text>
+                </View>
+
+                <View className="flex items-center mx-4 space-y-4">
+                    <Animated.View entering={FadeInDown.duration(1000).springify()} className="bg-black/5 p-5 rounded-2xl w-full">
+                        <TextInput placeholder='Email' placeholderTextColor={'gray'} />
+                    </Animated.View>
+                    <Animated.View entering={FadeInDown.delay(200).duration(1000).springify()} className="bg-black/5 p-5 rounded-2xl w-full mb-3">
+                        <TextInput placeholder='Password' placeholderTextColor={'gray'} secureTextEntry />
+                    </Animated.View>
+                    <Animated.View entering={FadeInDown.delay(400).duration(1000).springify()} className="w-full">
+                        <TouchableOpacity className="w-full bg-sky-400 rounded-2xl mb-3 py-3">
+                            <Text className="text-xl font-bold text-white text-center">
+                                Prijavi se
+                            </Text>
+                        </TouchableOpacity>
+                    </Animated.View>
+                    <Animated.View entering={FadeInDown.delay(600).duration(1000).springify()} className="flex-row justify-center">
+                        <Text>Nemate račun? </Text>
+                        <TouchableOpacity onPress={() => navigation.push("Signup")}>
+                            <Text className="text-sky-600">
+                                Registriraj se!
+                            </Text>
+                        </TouchableOpacity>
+                    </Animated.View>
+                </View>
+            </View>
+        </View >
+    )
+}
+
+export default Login
