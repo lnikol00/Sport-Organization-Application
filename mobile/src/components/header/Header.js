4bf73570a36014b20355d259fcd732d4a4b012a0
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-04-05 03:06:59 +0200

    Creating header and adding axios

---
+++ mobile/src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text, Image } from 'react-native'
+
+function Header() {
+    return (
+        <View className="flex justify-between items-center flex-row w-[100%] h-[60px] p-2 shadow-lg bg-yellow-300 sticky">
+            <Image source={require('../../../assets/grb2.png')} className="w-[45px] h-[45px]" />
+            <Text className='font-semibold text-lg p-2'>ULŠS</Text>
+        </View>
+    )
+}
+
+export default Header
