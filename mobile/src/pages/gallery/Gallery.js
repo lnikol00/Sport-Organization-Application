630985ab80faba7d88ba7d026cdd59df18fd4a6a
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-04-10 18:08:00 +0200

    Adding Header to Gallery Page

--- mobile/src/pages/gallery/Gallery.js
+++ mobile/src/pages/gallery/Gallery.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import Header from '../../components/header/Header'
 
 function Gallery() {
     return (
-        <View>
-            <Text>Gallery Page</Text>
-        </View>
+        <>
+            <Header />
+            <View>
+                <Text>Gallery Page</Text>
+            </View>
+        </>
+
     )
 }
 
