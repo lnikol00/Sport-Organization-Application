0a429e017cb1b01431fa2ccbddaeea65d9c33528
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-04-12 12:41:02 +0200

    Fixing countdown timer in home page

--- mobile/src/pages/home/Home.js
+++ mobile/src/pages/home/Home.js
@@ -7,77 +7,34 @@ import { listNews } from "../../Redux/Actions/NewsActions"
 
 function Home() {
 
-    const [timerDays, setTimerDays] = useState(0)
-    const [timerHours, setTimerHours] = useState(0)
-    const [timerMinutes, setTimerMinutes] = useState(0)
-    const [timerSeconds, setTimerSeconds] = useState(0)
-
-    let interval;
-
-    const startTimer = () => {
-        const countDownDate = new Date("August 10, 2024 17:00:00").getTime();
-
-        interval = setInterval(() => {
-            const now = new Date().getTime();
-
-            const distance = countDownDate - now;
-
-            const days = Math.floor(distance / (24 * 60 * 60 * 1000));
-            const hours = Math.floor((distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60));
-            const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60));
-            const seconds = Math.floor((distance % (60 * 1000)) / (1000));
-
-            if (distance < 0) {
-                clearInterval(interval.current);
-                setTimerDays(0);
-                setTimerHours(0);
-                setTimerMinutes(0);
-                setTimerSeconds(0);
-            } else {
-                setTimerDays(days);
-                setTimerHours(hours);
-                setTimerMinutes(minutes);
-                setTimerSeconds(seconds);
-            }
-        })
-    }
-
-    useEffect(() => {
-        // startTimer();
-        console.log("Prvi useEffect")
-    }, [])
-
     const dispatch = useDispatch()
     const newsList = useSelector(state => state.newsList)
 
     const { error, loading, news } = newsList;
 
     useEffect(() => {
-        // dispatch(listNews());
-        console.log("Drugi useEffect")
+        dispatch(listNews());
     }, [dispatch])
 
     return (
         <>
+
             <Header />
-            <View className="p-3">
-                <ClockDown
-                    timerDays={timerDays}
-                    timerHours={timerHours}
-                    timerMinutes={timerMinutes}
-                    timerSeconds={timerSeconds}
-                />
-                <Text className="text-center text-4xl pt-10">Novosti</Text>
-                <ScrollView>
-                    <View className="flex-wrap flex-row justify-between">
-                        {news && news.map((item) => {
-                            return (
-                                <Text key={item.id}>{item.title}</Text>
-                            )
-                        })}
+            <ScrollView>
+                <View className="p-3">
+                    <ClockDown />
+                    <Text className="text-center text-4xl pt-10">Novosti</Text>
+                    <View>
+                        <View className="flex-wrap flex-row justify-between">
+                            {news && news.map((item) => {
+                                return (
+                                    <Text key={item.id}>{item.title}</Text>
+                                )
+                            })}
+                        </View>
                     </View>
-                </ScrollView>
-            </View>
+                </View>
+            </ScrollView>
         </>
     )
 }
--- mobile/src/pages/home/clock-down/ClockDown.js
+++ mobile/src/pages/home/clock-down/ClockDown.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import CountDown from "react-native-countdown-component"
 
-function ClockDown({ timerDays, timerHours, timerMinutes, timerSeconds }) {
+function ClockDown() {
     return (
         <View>
             <View className="flex justify-center items-center gap-10 pt-5">
@@ -10,30 +11,16 @@ function ClockDown({ timerDays, timerHours, timerMinutes, timerSeconds }) {
                     <Text className='text-xl font-semibold' >Metković - Opuzen - Ploče</Text>
                 </View>
                 <View>
-                    <View>
-                        <View className="relative m-auto flex justify-end items-center flex-row gap-10">
-                            <View className='block'>
-                                <View className='flex justify-center items-center pb-5'>
-                                    <Text className='flex bg-yellow-300 m-1 h-[50px] w-[135px] text-xl border-black border-[1px] text-center pt-[10px]'>{timerDays}</Text>
-                                    <Text className="text-lg pt-2">Dana</Text>
-                                </View>
-                                <View className='flex justify-center items-center'>
-                                    <Text className='flex bg-yellow-300 m-1 h-[50px] w-[135px] text-xl border-black border-[1px] text-center pt-[10px]'>{timerHours}</Text>
-                                    <Text className="text-lg pt-2">Sati</Text>
-                                </View>
-                            </View>
-                            <View className='block'>
-                                <View className='flex justify-center items-center pb-5'>
-                                    <Text className='flex bg-yellow-300 m-1 h-[50px] w-[135px] text-xl border-black border-[1px] text-center pt-[10px]'>{timerMinutes}</Text>
-                                    <Text className="text-lg pt-2">Minuta</Text>
-                                </View>
-                                <View className='flex justify-center items-center'>
-                                    <Text className='flex bg-yellow-300 m-1 h-[50px] w-[135px] text-xl border-black border-[1px] text-center pt-[10px]'>{timerSeconds}</Text>
-                                    <Text className="text-lg pt-2">Sekundi</Text>
-                                </View>
-                            </View>
-                        </View>
-                    </View>
+                    <CountDown
+                        until={10390719}
+                        digitStyle={{
+                            backgroundColor: "#FFEA00",
+                            borderWidth: 1,
+                        }}
+                        onFinish={() => alert("Finished!")}
+                        timeLabels={{ d: "Dana", h: "Sati", m: 'Minuta', s: 'Sekundi' }}
+                        size={30}
+                    />
                 </View>
             </View>
         </View>
